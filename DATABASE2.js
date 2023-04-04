4e09699fa3b4c77806fc31ae05dfd41c446b3ee6
SyahirahMF <129152809+SyahirahMF@users.noreply.github.com>
2023-04-04 22:41:29 +0800

    Add files via upload

---
+++ DATABASE2.js
@@ -0,0 +1,71 @@
+let dbUsers = [
+
+    {
+        username: "NSF",
+        password : "123456",
+        name: "N S",
+        email: "b022110002@student.utem.edu.my"
+    }
+    ,
+    {
+        username: "NAF",
+        password : "789321",
+        name: "NF",
+        email: "b022110003@student.utem.edu.my"
+    }
+    ,
+    {
+        username: "MAF",
+        password : "453627",
+        name: "AF",
+        email: "b022110004@student.utem.edu.my"
+    }
+]
+
+function login1(reqUsername, reqpassword)
+{
+let matchUser = dbUsers.find(user => user.username == reqUsername)
+console.log(matchUser)
+}
+
+//try to login
+
+login1("NSF","password")
+
+function login2(reqUsername, reqPassword)
+{
+ let matchUser = dbUsers.find ( x => x.username == reqUsername)
+
+if(!matchUser) return "User not found!"
+if(matchUser.password == reqPassword)
+{
+    return matchUser
+}
+else
+{
+    return "Invalid password"
+}
+}
+
+//for(let i = 0; i <dbUsers.length; i++)
+//console.log(login("NSF","password"))
+//console.log(login("NAF","password"))
+
+function register(reqUsername, reqPassword, reqName, reqEmail)
+{
+    dbUsers.push({
+        username : reqUsername,
+        password : reqPassword,
+        name : reqName, 
+        email : reqEmail
+    })
+}
+
+console.log(login1("NSF","password"))
+console.log(login1("NAF","password"))
+console.log(login1("MAF","password"))
+
+register("utem", "password", "fkekk", "fkekk@m.com")
+
+console.log(login2("utem","password"))
+
